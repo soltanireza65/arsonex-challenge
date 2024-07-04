f1b75a894986c09c711ef2f023161c9760659c1c
Reza Soltani <soltanireza65@gmail.com>
2024-07-04 23:13:26 +0330

    refactor

---
+++ src/common/utils/swagger.utils.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication<any>) {
+  const options = new DocumentBuilder()
+    .setTitle('Currency Exchange API')
+    .setDescription('API for currency exchange rates')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,25 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AllExceptionsFilter } from 'common/exceptions/all-exception.filter';
+import { setupSwagger } from 'common/utils/swagger.utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule.register({ driver: 'in-memory' }),
   );
 
-  const options = new DocumentBuilder()
-    .setTitle('Currency Exchange API')
-    .setDescription('API for currency exchange rates')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
 
   const srv = app.get<ConfigService>(ConfigService);
   const PORT = srv.get<number>('PORT');
+  
+  setupSwagger(app);
 
   app.useGlobalPipes();
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+  
   await app.listen(PORT);
 }
 bootstrap();
