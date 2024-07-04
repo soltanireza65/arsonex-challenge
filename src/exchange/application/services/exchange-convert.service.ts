001c1c11969094a80d04b292fc4d33906d04f795
Reza Soltani <soltanireza65@gmail.com>
2024-07-04 22:43:24 +0330

    Revert "convertion controller compledetd"
    This reverts commit dd60082a8063627c98ff76277f56210389305dd1.

--- src/exchange/application/services/exchange-convert.service.ts
+++ src/exchange/application/services/exchange-convert.service.ts
@@ -1,30 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
-import { ConvertCryptoTyCryptoQuery } from '../queries/convert/convert-crypto-to-crypto.query';
-import { ConvertEURToCryptoQuery } from '../queries/convert/convert-eur-to-crypto.query';
-import { ConvertIRRToUSDQuery } from '../queries/convert/convert-irr-to-usd.query';
-import { ConvertUSDToFiatQuery } from '../queries/convert/convert-usd-to-fiat.query';
 
 @Injectable()
-export class ExchangeConvertService {
-  constructor(private readonly queryBus: QueryBus) {}
-
-  async convertIRRToUSD(query: ConvertIRRToUSDQuery): Promise<number> {
-    return this.queryBus.execute(new ConvertIRRToUSDQuery(query.amount));
-  }
-
-  async convertUSDToOther(query: ConvertUSDToFiatQuery) {
-    const { to, amount } = query;
-    return this.queryBus.execute(new ConvertUSDToFiatQuery(to, amount));
-  }
-  async convertCryptoToCrypto(query: ConvertCryptoTyCryptoQuery) {
-    const { from, to, amount } = query;
-    return this.queryBus.execute(
-      new ConvertCryptoTyCryptoQuery(from, to, amount),
-    );
-  }
-  async convertEURToCrypto(query: ConvertEURToCryptoQuery) {
-    const { to, amount } = query;
-    return this.queryBus.execute(new ConvertEURToCryptoQuery(to, amount));
-  }
-}
+export class ExchangeConvertService {}
--- src/exchange/interface/http/exchange-convert.controller.ts
+++ src/exchange/interface/http/exchange-convert.controller.ts
@@ -15,27 +15,27 @@ export class ExchangeConvertionController {
   @ApiResponse({ status: 200, description: 'The conversion was successful.' })
   @Get('irr-to-usd')
   async convertIRRToUSD(@Query() args: ConvertIRRToUSD) {
-    return this.service.convertIRRToUSD(args);
+    throw new Error('Not implemented');
   }
 
   @ApiOperation({ summary: 'Convert USD to other Fiat currencies' })
   @ApiResponse({ status: 200, description: 'The conversion was successful.' })
   @Get('usd-to-other')
   async convertUSDToOther(@Query() args: ConvertUSDToFiat) {
-    return this.service.convertUSDToOther(args);
+    throw new Error('Not implemented');
   }
 
   @ApiOperation({ summary: 'Convert Crypto to Crypto' })
   @ApiResponse({ status: 200, description: 'The conversion was successful.' })
   @Get('crypto-to-crypto')
   async convertCryptoToCrypto(@Query() args: ConvertCryptoTyCrypto) {
-    return this.service.convertCryptoToCrypto(args);
+    throw new Error('Not implemented');
   }
 
   @ApiOperation({ summary: 'Convert EUR to Crypto' })
   @ApiResponse({ status: 200, description: 'The conversion was successful.' })
   @Get('eur-to-crypto')
   async convertEURToCrypto(@Query() args: ConvertEURToCrypto) {
-    return this.service.convertEURToCrypto(args);
+    throw new Error('Not implemented');
   }
 }
