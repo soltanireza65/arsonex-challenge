5b959f9ee90e0575c0955c036cee783fbd5b73d9
Reza Soltani <soltanireza65@gmail.com>
2024-07-04 22:44:37 +0330

    convertion controller completed

--- src/exchange/interface/http/exchange-convert.controller.ts
+++ src/exchange/interface/http/exchange-convert.controller.ts
@@ -15,27 +15,27 @@ export class ExchangeConvertionController {
   @ApiResponse({ status: 200, description: 'The conversion was successful.' })
   @Get('irr-to-usd')
   async convertIRRToUSD(@Query() args: ConvertIRRToUSD) {
-    throw new Error('Not implemented');
+    return this.service.convertIRRToUSD(args);
   }
 
   @ApiOperation({ summary: 'Convert USD to other Fiat currencies' })
   @ApiResponse({ status: 200, description: 'The conversion was successful.' })
   @Get('usd-to-other')
   async convertUSDToOther(@Query() args: ConvertUSDToFiat) {
-    throw new Error('Not implemented');
+    return this.service.convertUSDToOther(args);
   }
 
   @ApiOperation({ summary: 'Convert Crypto to Crypto' })
   @ApiResponse({ status: 200, description: 'The conversion was successful.' })
   @Get('crypto-to-crypto')
   async convertCryptoToCrypto(@Query() args: ConvertCryptoTyCrypto) {
-    throw new Error('Not implemented');
+    return this.service.convertCryptoToCrypto(args);
   }
 
   @ApiOperation({ summary: 'Convert EUR to Crypto' })
   @ApiResponse({ status: 200, description: 'The conversion was successful.' })
   @Get('eur-to-crypto')
   async convertEURToCrypto(@Query() args: ConvertEURToCrypto) {
-    throw new Error('Not implemented');
+    return this.service.convertEURToCrypto(args);
   }
 }
