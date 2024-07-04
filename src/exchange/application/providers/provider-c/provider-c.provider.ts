149b6f8107e0501d4de055c5d61f6c11d510b4ab
Reza Soltani <soltanireza65@gmail.com>
2024-07-04 17:31:15 +0330

    third provider

---
+++ src/exchange/application/providers/provider-c/provider-c.provider.ts
@@ -0,0 +1,38 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ExchangeRateProvider } from 'exchange/application/ports/exchange-rate.provider';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class ProviderCProvider extends ExchangeRateProvider {
+  private baseURL: string;
+
+  constructor(
+    protected readonly httpService: HttpService,
+    protected readonly configService: ConfigService,
+  ) {
+    super(httpService, configService);
+
+    this.baseURL = configService.get<string>('COINGATE_BASE_URL');
+  }
+
+  async calculate(from: string, to: string): Promise<number> {
+    try {
+      const obs = await this.httpService.get(
+        `api/v2/rates/merchant/${from}/${to}`,
+        {
+          baseURL: this.baseURL,
+          params: {
+            vs_currencies: from,
+            ids: to,
+          },
+        },
+      );
+
+      const response = await lastValueFrom(obs);
+
+      return response.data;
+    } catch (error) {}
+  }
+}
---
+++ src/exchange/application/providers/provider.ts
@@ -0,0 +1,3 @@
+export { ProviderAProvider } from './provider-a/provider-a.provider';
+export { ProviderBProvider } from './provider-b/provider-b.provider';
+export { ProviderCProvider } from './provider-c/provider-c.provider';
