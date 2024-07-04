b7118ab2f43f509e4b3f906d9fcffa8318842ba1
Reza Soltani <soltanireza65@gmail.com>
2024-07-05 01:28:17 +0330

    ExchangeRateController unit tests

--- src/exchange/interface/http/exchange-rate.controller.spec.ts
+++ src/exchange/interface/http/exchange-rate.controller.spec.ts
@@ -7,13 +7,22 @@ describe('ExchangeRateController', () => {
   let service: Partial<ExchangeRateService>;
 
   beforeEach(async () => {
-    // service = new ExchangeRateService(null);
     service = {
-      findAll: () => { return Promise.resolve([]) },
-      rateIRRToUSD: () => { return Promise.resolve(1) },
-      rateUSDToOther: () => { return Promise.resolve(1) },
-      rateCryptoToCrypto: () => { return Promise.resolve(1) },
-      rateEURToCrypto: () => { return Promise.resolve(1) },
+      findAll: () => {
+        return Promise.resolve([]);
+      },
+      rateIRRToUSD: () => {
+        return Promise.resolve(1);
+      },
+      rateUSDToOther: () => {
+        return Promise.resolve(1);
+      },
+      rateCryptoToCrypto: () => {
+        return Promise.resolve(1);
+      },
+      rateEURToCrypto: () => {
+        return Promise.resolve(1);
+      },
     } as Partial<ExchangeRateService>;
 
     const app: TestingModule = await Test.createTestingModule({
@@ -27,6 +36,30 @@ describe('ExchangeRateController', () => {
   describe('findAll', () => {
     it('should return an array of exchange rates', async () => {
       expect(await controller.findAll()).toEqual([]);
+    });
+  });
+
+  describe('rateIRRToUSD', () => {
+    it('should return an exchange rate', async () => {
+      expect(await controller.rateIRRToUSD({})).toEqual(1);
+    });
+  });
+
+  describe('rateUSDToOther', () => {
+    it('should return an exchange rate', async () => {
+      expect(await controller.rateUSDToOther({to:"USD"})).toEqual(1);
+    });
+  });
+
+  describe('rateCryptoToCrypto', () => {
+    it('should return an exchange rate', async () => {
+      expect(await controller.rateCryptoToCrypto({from:"BTC",to:"USD"})).toEqual(1);
+    });
+  });
+
+  describe('rateEURToCrypto', () => {
+    it('should return an exchange rate', async () => {
+      expect(await controller.rateEURToCrypto({to:"BTC"})).toEqual(1);
     });
   });
 });
