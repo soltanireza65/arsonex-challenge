590a01fa6b732db5db0ff2f32c677df11e406946
Reza Soltani <soltanireza65@gmail.com>
2024-07-04 17:34:57 +0330

    setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule.register({ driver: 'in-memory' }),
   );
 
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('Currency Exchange API')
+    .setDescription('API for currency exchange rates')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  const srv = app.get<ConfigService>(ConfigService);
+  const PORT = srv.get<number>('PORT');
+
+  app.useGlobalPipes();
+  await app.listen(PORT);
 }
 bootstrap();
